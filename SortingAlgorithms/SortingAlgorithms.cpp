5f6566d1d393351ec53f8643b79b068c48adef84
Nikita Reznikov <63803175+Thermazote@users.noreply.github.com>
2022-05-20 16:57:23 +0300

    Update SortingAlgorithms.cpp

--- SortingAlgorithms/SortingAlgorithms.cpp
+++ SortingAlgorithms/SortingAlgorithms.cpp
@@ -38,6 +38,22 @@ int main()
     std::vector<method_t> methods(1);
     methods[0].name = "Selection sort";
     methods[0].function = &Sorter::selection_sort;
+    methods[1].name = "Insertion sort";
+    methods[1].function = &Sorter::insertion_sort;
+    methods[2].name = "Merge sort";
+    methods[2].function = &Sorter::merge_sort;
+    methods[3].name = "Heap sort";
+    methods[3].function = &Sorter::heap_sort;
+    methods[4].name = "Quick sort";
+    methods[4].function = &Sorter::quick_sort;
+    methods[5].name = "Bucket sort";
+    methods[5].function = &Sorter::bucket_sort;
+    methods[6].name = "Bubble sort";
+    methods[6].function = &Sorter::bubble_sort;
+    methods[7].name = "Counting sort";
+    methods[7].function = &Sorter::counting_sort;
+    methods[8].name = "Radix sort";
+    methods[8].function = &Sorter::radix_sort;
 
     // open the file the results will be written to
     std::fstream file;
